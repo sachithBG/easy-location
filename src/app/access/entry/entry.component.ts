507bbd161e470079ed99291ab0a27c7694996d59
sachithBG <sachithguruge@gmail.com>
2021-04-18 22:54:13 +0530

    all completed

--- src/app/access/entry/entry.component.ts
+++ src/app/access/entry/entry.component.ts
@@ -1,3 +1,4 @@
+import { trigger, transition, style, animate } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { SelectLocComponent } from '../select-loc/select-loc.component';
@@ -5,7 +6,15 @@ import { SelectLocComponent } from '../select-loc/select-loc.component';
 @Component({
   selector: 'app-entry',
   templateUrl: './entry.component.html',
-  styleUrls: ['./entry.component.scss']
+  styleUrls: ['./entry.component.scss'],
+  animations: [
+    trigger('fade', [ 
+      transition('void => *', [
+        style({ opacity: 0 }), 
+        animate(1000, style({opacity: 1}))
+      ]) 
+    ])
+  ]
 })
 export class EntryComponent implements OnInit {
 
